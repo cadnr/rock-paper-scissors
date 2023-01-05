88e5edcb9515f833813b349f3dc92035403affd3
nighfy <82936194+nighfy@users.noreply.github.com>
2023-01-05 14:33:19 +0100

    Add function playRound

--- javascript.js
+++ javascript.js
@@ -14,3 +14,42 @@ function getPlayerChoice() {
         if(input == 'rock' || input == 'paper' || input == 'scissors') return input;
     }
 }
+
+function playRound() {
+    let computerChoice, playerChoice;
+
+    computerChoice = getComputerChoice();
+    playerChoice = getPlayerChoice();
+
+    console.log(computerChoice, playerChoice);
+
+    if(computerChoice == 'rock') {
+        if(playerChoice == 'rock') {
+            return "It's a tie!"
+        } else if (playerChoice == 'paper') {
+            return "Player wins!"
+        } else if (playerChoice == 'scissors') {
+            return "Computer wins!"
+        }
+    }
+
+    if(computerChoice == 'paper') {
+        if(playerChoice == 'paper') {
+            return "It's a tie!"
+        } else if (playerChoice == 'scissors') {
+            return "Player wins!"
+        } else if (playerChoice == 'rock') {
+            return "Computer wins!"
+        }
+    }
+
+        if(computerChoice == 'scissors') {
+            if(playerChoice == 'scissors') {
+                return "It's a tie!"
+            } else if (playerChoice == 'rock') {
+                return "Player wins!"
+            } else if (playerChoice == 'paper') {
+                return "Computer wins!"
+            }
+        }
+}
